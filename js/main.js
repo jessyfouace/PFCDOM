10223bb95b3351a44afe446b637365ad2edc8de9
Jessy Fouace <jessy.fouace62@gmail.com>
2018-08-08 11:08:03 +0200

    Base playing without animation on attack and no responsive spawn

--- js/main.js
+++ js/main.js
@@ -1,88 +1,176 @@
 // Tables for choise of 3
-var choice = ["pierre", "papier", "ciseau"];
+var choice = ["carapuce", "salameche", "bulbizar"];
 // Number of start life of player
 var playerpoint = 0;
 // Number of start life of adversary
 var adversarypoint = 0;
 
+// Choise pokemon on click
+document.getElementById("choisebulbi").onclick = function() {bulbi()};
+document.getElementById("choisesala").onclick = function() {sala()};
+document.getElementById("choisecara").onclick = function() {cara()};
+
 // while loop from the moment the life of the bot or the life of the player is not 3
-while(playerpoint <= 2 && adversarypoint <= 2) {
-
-// Random choise of bot
-  var bot = [Math.floor(Math.random() * choice.length)];
-
-// Choise of player
-  // var playerchoice = prompt("Choisissez entre Pierre Papier ou Ciseau");
-  playerchoice = playerchoice.toLowerCase();
-
-// Categories of "Pierre"
-  // Rock vs Rock
-  if (playerchoice == choice[0] && bot == 0) {
-     console.log("Egalité le bot a choisis Pierre");
-   }
-  // Rock vs Paper
-  if (playerchoice == choice[0]  && bot == 1) {
-    console.log("Le Bot a choisis Papier, vous avez perdu cette manche");
-    adversarypoint = adversarypoint + 1;
-    console.log("L'adversaire a " + adversarypoint + " point(s)");
-  }
-  // Rock vs Cisor
-  if (playerchoice == choice[0] && bot == 2) {
-    console.log("Vous avez gagner un point !");
-    playerpoint = playerpoint + 1;
-    console.log("Vous avez " + playerpoint + " point(s)");
-  }
-
-// Categories of "Papier"
-  // Paper vs Rock
-  if (playerchoice == choice[1] && bot == 0) {
-     console.log("Vous avez gagner un point !");
-     playerpoint = playerpoint + 1;
-     console.log("Vous avez " + playerpoint + " point(s)");
-   }
-  // Paper vs Paper
-  if (playerchoice == choice[1] && bot == 1) {
-    console.log("Egalité le bot a choisis Papier");
-  }
-  // Paper vs Cisor
-  if (playerchoice == choice[1] && bot == 2) {
-    console.log("Le Bot a choisis Ciseau, vous avez perdu cette manche");
-    adversarypoint = adversarypoint + 1;
-    console.log("L'adversaire a " + adversarypoint + " point(s)");
-  }
 
 
-// Categories of "Ciseau"
-  // Cisor vs rock
-  if (playerchoice == choice[2] && bot == 0) {
-     console.log("Le Bot a choisis Pierre, vous avez perdu cette manche");
-     adversarypoint = adversarypoint + 1;
-     console.log("L'adversaire a " + adversarypoint + " point(s)");
-   }
-  // Cisor vs paper
-  if (playerchoice == choice[2] && bot == 1) {
-    console.log("Vous avez gagner un point !");
-    playerpoint = playerpoint + 1;
-    console.log("Vous avez " + playerpoint + " point(s)");
-  }
-  // Cisor vs Cisor
-  if (playerchoice == choice[2] && bot == 2) {
-    console.log("Egalité le bot a choisis Ciseau");
+  function cara(){
+    var bot = [Math.floor(Math.random() * choice.length)];
+    if (bot == 0){
+      document.getElementById("carapucechoise").style.display="inline-block";
+      document.getElementById("salamechechoise").style.display="none";
+      document.getElementById("bulbizarchoise").style.display="none";
+      document.getElementById("carabot").style.display="inline-block";
+      document.getElementById("salabot").style.display="none";
+      document.getElementById("bulbibot").style.display="none";
+    }
+    else if (bot == 1){
+      document.getElementById("carapucechoise").style.display="inline-block";
+      document.getElementById("salamechechoise").style.display="none";
+      document.getElementById("bulbizarchoise").style.display="none";
+      document.getElementById("carabot").style.display="none";
+      document.getElementById("salabot").style.display="inline-block";
+      document.getElementById("bulbibot").style.display="none";
+      playerpoint = playerpoint +1;
+    }
+    else if (bot == 2){
+      document.getElementById("carapucechoise").style.display="inline-block";
+      document.getElementById("salamechechoise").style.display="none";
+      document.getElementById("bulbizarchoise").style.display="none";
+      document.getElementById("carabot").style.display="none";
+      document.getElementById("salabot").style.display="none";
+      document.getElementById("bulbibot").style.display="inline-block";
+      adversarypoint = adversarypoint +1;
+    }
+    if (adversarypoint == 1) {
+      document.getElementById("progressbarplayer").style.width="66%";
+      document.getElementById("progressbarplayer").innerHTML="2 vie";
+    } else if (adversarypoint == 2) {
+      document.getElementById("progressbarplayer").style.width="33%";
+      document.getElementById("progressbarplayer").innerHTML="1 vie";
+    } else if (adversarypoint == 3) {
+      document.getElementById("progressbarplayer").style.width="0%";
+      document.getElementById("restartall").style.display="inline-block";
+      document.getElementById("progressbarplayer").innerHTML="";
+      document.getElementById("testv").innerHTML="Vous ne batterez jamais la team Plasma !";
+    }
+    if (playerpoint == 1) {
+      document.getElementById("progressbarbot").style.width="66%";
+      document.getElementById("progressbarbot").innerHTML="2 vie";
+    } else if (playerpoint == 2) {
+      document.getElementById("progressbarbot").style.width="33%";
+      document.getElementById("progressbarbot").innerHTML="1 vie";
+    } else if (playerpoint == 3) {
+      document.getElementById("progressbarbot").style.width="0%";
+      document.getElementById("restartall").style.display="inline-block";
+      document.getElementById("progressbarbot").innerHTML="";
+      document.getElementById("testv").innerHTML="Vous venez de battre la team Plasma ! Félicitation."
+    }
   }
 
-// If the player does not put good value in the prompt
-  else if (playerchoice !== choice[0] && playerchoice !== choice[1] && playerchoice !== choice[2]) {
-    console.log("Entrez un bon choix")
+  function sala(){
+    var bot = [Math.floor(Math.random() * choice.length)];
+    if (bot == 0){
+      document.getElementById("carapucechoise").style.display="none";
+      document.getElementById("salamechechoise").style.display="inline-block";
+      document.getElementById("bulbizarchoise").style.display="none";
+      document.getElementById("carabot").style.display="inline-block";
+      document.getElementById("salabot").style.display="none";
+      document.getElementById("bulbibot").style.display="none";
+      adversarypoint = adversarypoint +1;
+    }
+    else if (bot == 1){
+      document.getElementById("carapucechoise").style.display="none";
+      document.getElementById("salamechechoise").style.display="inline-block";
+      document.getElementById("bulbizarchoise").style.display="none";
+      document.getElementById("carabot").style.display="none";
+      document.getElementById("salabot").style.display="inline-block";
+      document.getElementById("bulbibot").style.display="none";
+    }
+    else if (bot == 2){
+      document.getElementById("carapucechoise").style.display="none";
+      document.getElementById("salamechechoise").style.display="inline-block";
+      document.getElementById("bulbizarchoise").style.display="none";
+      document.getElementById("carabot").style.display="none";
+      document.getElementById("salabot").style.display="none";
+      document.getElementById("bulbibot").style.display="inline-block";
+      playerpoint = playerpoint +1;
+    }
+    if (adversarypoint == 1) {
+      document.getElementById("progressbarplayer").style.width="66%";
+      document.getElementById("progressbarplayer").innerHTML="2 vie";
+    } else if (adversarypoint == 2) {
+      document.getElementById("progressbarplayer").style.width="33%";
+      document.getElementById("progressbarplayer").innerHTML="1 vie";
+    } else if (adversarypoint == 3) {
+      document.getElementById("progressbarplayer").style.width="0%";
+      document.getElementById("restartall").style.display="inline-block";
+      document.getElementById("progressbarplayer").innerHTML="";
+      document.getElementById("testv").innerHTML="Vous ne batterez jamais la team Plasma !";
+    }
+    if (playerpoint == 1) {
+      document.getElementById("progressbarbot").style.width="66%";
+      document.getElementById("progressbarbot").innerHTML="2 vie";
+    } else if (playerpoint == 2) {
+      document.getElementById("progressbarbot").style.width="33%";
+      document.getElementById("progressbarbot").innerHTML="1 vie";
+    } else if (playerpoint == 3) {
+      document.getElementById("progressbarbot").style.width="0%";
+      document.getElementById("restartall").style.display="inline-block";
+      document.getElementById("progressbarbot").innerHTML="";
+      document.getElementById("testv").innerHTML="Vous venez de battre la team Plasma ! Félicitation."
+    }
   }
 
-}
-
-// Message if the player wins
-if (playerpoint >= 3) {
-  console.log("Vous avez gagner cette partie");
-}
-
-// Message if the bot wins
-if (adversarypoint >= 3) {
-  console.log("Vous avez Perdu cette partie");
-}
+  function bulbi(){
+    var bot = [Math.floor(Math.random() * choice.length)];
+    if (bot == 0){
+      document.getElementById("carapucechoise").style.display="none";
+      document.getElementById("salamechechoise").style.display="none";
+      document.getElementById("bulbizarchoise").style.display="inline-block";
+      document.getElementById("carabot").style.display="inline-block";
+      document.getElementById("salabot").style.display="none";
+      document.getElementById("bulbibot").style.display="none";
+      playerpoint = playerpoint +1;
+    }
+    else if (bot == 1){
+      document.getElementById("carapucechoise").style.display="none";
+      document.getElementById("salamechechoise").style.display="none";
+      document.getElementById("bulbizarchoise").style.display="inline-block";
+      document.getElementById("carabot").style.display="none";
+      document.getElementById("salabot").style.display="inline-block";
+      document.getElementById("bulbibot").style.display="none";
+      adversarypoint = adversarypoint +1;
+    }
+    else if (bot == 2){
+      document.getElementById("carapucechoise").style.display="none";
+      document.getElementById("salamechechoise").style.display="none";
+      document.getElementById("bulbizarchoise").style.display="inline-block";
+      document.getElementById("carabot").style.display="none";
+      document.getElementById("salabot").style.display="none";
+      document.getElementById("bulbibot").style.display="inline-block";
+    }
+    if (adversarypoint == 1) {
+      document.getElementById("progressbarplayer").style.width="66%";
+      document.getElementById("progressbarplayer").innerHTML="2 vie";
+    } else if (adversarypoint == 2) {
+      document.getElementById("progressbarplayer").style.width="33%";
+      document.getElementById("progressbarplayer").innerHTML="1 vie";
+    } else if (adversarypoint == 3) {
+      document.getElementById("progressbarplayer").style.width="0%";
+      document.getElementById("restartall").style.display="inline-block";
+      document.getElementById("progressbarplayer").innerHTML="";
+      document.getElementById("testv").innerHTML="Vous ne batterez jamais la team Plasma !";
+    }
+    if (playerpoint == 1) {
+      document.getElementById("progressbarbot").style.width="66%";
+      document.getElementById("progressbarbot").innerHTML="2 vie";
+    } else if (playerpoint == 2) {
+      document.getElementById("progressbarbot").style.width="33%";
+      document.getElementById("progressbarbot").innerHTML="1 vie";
+    } else if (playerpoint == 3) {
+      document.getElementById("progressbarbot").style.width="0%";
+      document.getElementById("restartall").style.display="inline-block";
+      document.getElementById("progressbarbot").innerHTML="";
+      document.getElementById("testv").innerHTML="Vous venez de battre la team Plasma ! Félicitation."
+    }
+  }
